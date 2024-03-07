7c35d77ac1c898e3ff1a1b87414ea3c2e61a324b
Ana Carolina <128608586+anabregolin@users.noreply.github.com>
2024-03-06 21:41:00 -0300

    Update ex07.vhd

--- ex07.vhd
+++ ex07.vhd
@@ -3,13 +3,13 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY half_sum IS
-	PORT(	a, b	: IN STD_LOGIC;
-		s, c	: OUT STD_LOGIC);
+	PORT(	a, b		: IN STD_LOGIC;
+		sigma, cout	: OUT STD_LOGIC);
 END half_sum;
 
 ARCHITECTURE df OF half_sum IS
 
 BEGIN
-	s <= (not a) or (not b);
-	c <= a and b;
+	sigma <= a xor b;
+	cout  <= a and b;
 END df;
